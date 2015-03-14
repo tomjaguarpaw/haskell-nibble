7d129a5f02892d0378214bec1e7e131169ecb143
Tom Ellis <tom-2013@purelyagile.com>
2015-03-14 17:52:13 +0000

    Switch to new directory style.  Dynamic.

--- Nibble.hs
+++ Nibble.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Nibble where
 
 import qualified Control.Monad.Trans.Reader as R
@@ -30,45 +32,69 @@ liftIO = N . lift
 
 data Params = Params { pHeapDir :: String }
 
-data Object a = HODirectory (Directory a)
-              | HOFile (File a)
-
 data Heap
 data PreHeap
 
-data Directory a = Directory String deriving Show
-data File a      = File String
+data Directory s a = Directory String deriving Show
 
-data PackageDB a = PackageDB (Directory a) deriving Show
-data PackageDBList = PackageDBList (Directory Heap) deriving Show
+data PackageDB
+data PackageDBList
+data Raw
+data Dynamic
 
+data TypeRep = TypeRep String
 
-emptyDirectoryPreHeap :: N (Directory PreHeap)
+class Typeable a where
+  typeRep :: proxy a -> TypeRep
+
+toDyn :: Typeable a => Directory Heap a -> N (Directory Heap Dynamic)
+toDyn a = do
+  d <- emptyDirectoryPreHeap
+  createHeapLink d "obj" a
+  let dir = directoryFilename d
+  let TypeRep typerep = typeRep a
+  liftIO (IO.writeFile (dir ++ "/TypeRep") typerep)
+  stampDirectory (unsafeCoerce d)
+
+fromDyn :: forall a. Typeable a => Directory Heap Dynamic -> N (Maybe (Directory Heap a))
+fromDyn a = do
+  let dir = directoryFilename a
+      ret = unsafeCoerceHeap a
+      TypeRep typerep' = typeRep (ret :: Directory Heap a)
+  typerep <- liftIO (IO.readFile (dir ++ "/TypeRep"))
+  if typerep == typerep'
+    then return (Just ret)
+    else return Nothing
+
+unsafeCoerce :: Directory PreHeap a -> Directory PreHeap b
+unsafeCoerce (Directory d) = Directory d
+
+unsafeCoerceHeap :: Directory Heap a -> Directory Heap b
+unsafeCoerceHeap (Directory d) = Directory d
+
+emptyDirectoryPreHeap :: N (Directory PreHeap Raw)
 emptyDirectoryPreHeap = do
   heapDir <- fmap pHeapDir ask
   liftIO $ do
     newDir <- T.createTempDirectory heapDir "heapObject"
     return (Directory newDir)
   
-createFile :: Directory PreHeap -> String -> String -> N ()
+-- Could switch Raw to a
+createFile :: Directory PreHeap Raw -> String -> String -> N ()
 createFile (Directory dir) filename = liftIO . IO.writeFile (dir ++ "/" ++ filename) 
 
-createEmptyPackageDB :: N (PackageDB PreHeap)
+createEmptyPackageDB :: N (Directory PreHeap PackageDB)
 createEmptyPackageDB = do
   d <- emptyDirectoryPreHeap
   createFile d "package.conf" "[]"
-  return (PackageDB d)
+  return (unsafeCoerce d)
 
-objectFilename :: Object a -> String
-objectFilename (HODirectory d) = directoryFilename d
-objectFilename (HOFile (File n)) = n
+directoryFilename :: Directory s a -> String
+directoryFilename (Directory d) = d
 
-directoryFilename :: Directory a -> String
-directoryFilename (Directory n) = n
-
-createHeapLink :: Directory PreHeap -> String -> Object Heap -> N ()
-createHeapLink (Directory dir) linkName object =
-  liftIO (PF.createSymbolicLink (objectFilename object) (dir ++ "/" ++ linkName))
+createHeapLink :: Directory PreHeap a -> String -> Directory Heap b -> N ()
+createHeapLink (Directory dir) linkName target =
+  liftIO (PF.createSymbolicLink (directoryFilename target) (dir ++ "/" ++ linkName))
 
 runN :: Params -> N a -> IO a
 runN p (N a) = R.runReaderT a p
@@ -76,28 +102,23 @@ runN p (N a) = R.runReaderT a p
 runTest :: N a -> IO a
 runTest = runN Params { pHeapDir = "/home/tom/tmp-nibble-heap" }
 
-storePackageDBList :: [PackageDB Heap] -> N PackageDBList
+storePackageDBList :: [Directory Heap PackageDB] -> N (Directory Heap PackageDBList)
 storePackageDBList packagedbs = do
   dir <- emptyDirectoryPreHeap
 
-  F.forM_ (zip [1 :: Int ..] packagedbs) $ \(i, PackageDB d) ->
-      createHeapLink dir (show i) (HODirectory d)
+  F.forM_ (zip [1 :: Int ..] packagedbs) $ \(i, d) ->
+      createHeapLink dir (show i) d
 
-  dirStamped <- stampDirectory dir
-  return (PackageDBList dirStamped)
+  dirStamped <- stampDirectory (unsafeCoerce dir)
+  return dirStamped
 
-stampDirectory :: Directory PreHeap -> N (Directory Heap)
+stampDirectory :: Directory PreHeap a -> N (Directory Heap a)
 stampDirectory (Directory d) = return (Directory d)
 
-stampPackageDB :: PackageDB PreHeap -> N (PackageDB Heap)
-stampPackageDB (PackageDB d) = do
-  s <- stampDirectory d
-  return (PackageDB s)
+packageConf :: Directory a PackageDB -> String
+packageConf (Directory d) = d ++ "/" ++ "package.conf"
 
-packageConf :: PackageDB a -> String
-packageConf (PackageDB (Directory d)) = d ++ "/" ++ "package.conf"
-
-cabalConfigureBuildInstall :: [PackageDB Heap] -> N (PackageDB Heap)
+cabalConfigureBuildInstall :: [Directory Heap PackageDB] -> N (Directory Heap PackageDB)
 cabalConfigureBuildInstall packagedbs = do
   let packageConfs = concatMap (\packagedb -> " --package-db " ++ packageConf packagedb) packagedbs
       configure = "cabal configure" ++ packageConfs
@@ -107,7 +128,7 @@ cabalConfigureBuildInstall packagedbs = do
     ignoreFIXME $ P.system configure
     ignoreFIXME $ P.system build
 
-  newpackagedb@(PackageDB newpackagedbdir) <- createEmptyPackageDB
+  newpackagedb <- createEmptyPackageDB
   newdeployment <- emptyDirectoryPreHeap
   
   let extrapackageConf = " --package-db " ++ packageConf newpackagedb
@@ -116,9 +137,9 @@ cabalConfigureBuildInstall packagedbs = do
 
   ignoreFIXME (liftIO (P.system install))
 
-  mapM_ (\(i, PackageDB d) -> createHeapLink newpackagedbdir (show i) (HODirectory d)) (zip [1 :: Int ..] packagedbs)
+  mapM_ (\(i, d) -> createHeapLink newpackagedb (show i) d) (zip [1 :: Int ..] packagedbs)
 
-  stampPackageDB newpackagedb
+  stampDirectory newpackagedb
 
 ignoreFIXME :: Functor m => m a -> m ()
 ignoreFIXME = fmap (const ())
